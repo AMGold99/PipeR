6ddc89ec56dfd2e598539ec673b8eeef0fbc1065
Asa Gold <gold1@stolaf.edu>
2022-03-08 15:21:55 -0600

    add imports

--- misc/packages_import.R
+++ misc/packages_import.R
@@ -1,6 +1,6 @@
 # required packages
 
-packages <- c("dplyr","plyr","forcats","ggplot2","ggrepel","prob","cowplot","stringr","tidyr", "magrittr", "tibble")
+packages <- c("dplyr","plyr","forcats","ggplot2","ggrepel","prob","cowplot","stringr","tidyr", "magrittr", "tibble","fs","purrr")
 versions <- lapply(packages, packageVersion)
 
 for(i in seq_len(length(packages))) {
