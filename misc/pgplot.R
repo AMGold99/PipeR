41c5395613219a88767eb003f7c566336e5264ba
Asa Gold <gold1@stolaf.edu>
2022-03-09 22:38:15 -0600

    delete pgplot scratchwork

--- misc/pgplot.R
+++
@@ -1,6 +0,0 @@
-# vars for pgplot()
-# data, x-axis var, y-axis var, group-var (eg, Ethnicity)
-# facet var (eg, Value),
-# year var (.data[[]]? this is for the x-axis breaks)
-# Title, subtitle, ylab, xlab default these to "DEFAULT..."
-# palette
